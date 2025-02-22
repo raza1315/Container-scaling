3346a69a77907518106cbe72fa480af044ead53f
raza1315 <razasfs2003@gmail.com>
2025-02-22 13:22:16 +0530

    heavytask added

--- index.js
+++ index.js
@@ -11,3 +11,10 @@ console.log("hostname:",os.hostname());
 res.status(200).json(`Server on Host ${os.hostname()}:${PORT}`);
 })
 
+app.get("/heavytask",(req,res)=>{
+let sum=0;
+for (let i=0;i<=10000000000;i++){
+sum+=i;
+}
+res.status(200).json(`sum is ${sum} given by ${os.hostname()}`);
+})
